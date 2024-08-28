5c6d70b88d763257d4037e8bdf521b6014751029
Bobby Naluz <bnaluz9@gmail.com>
2024-08-28 19:07:22 -0400

    fix:migrations environ variables

--- migrations/versions/20240823_135241_.py
+++ migrations/versions/20240823_135241_.py
@@ -8,6 +8,10 @@ Create Date: 2024-08-23 13:52:41.394823
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = '388ef177dc2a'
